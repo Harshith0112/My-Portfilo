de65a2036eb69f5f57645b2abe20c9b4b7d19566
Dindukurthi Harshith <dindukurthiharshith@gmail.com>
2025-05-20 23:32:28 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -33,8 +33,8 @@ function App() {
       <About />
       <Education />
       <Skills />
-      <Projects />
       <Certifications />
+      <Projects />
       <Contact />
       <Footer />
     </ThemeProvider>
